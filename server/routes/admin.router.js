f90d5ca5d8440cc68e3685f4f68ccc9452d946b3
Jeff Richardson <jjrich13@gmail.com>
2018-09-24 16:56:27 -0500

    Nodemailer stuff and new build

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -2,8 +2,35 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const { dateToString } = require('../modules/dateToString');
+const nodemailer = require('nodemailer');
+
+// NODEMAILER STUFF THAT YOU MIGHT WANT TO MOVE
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    },
+    // for handling request from local host 
+    tls: {
+        rejectUnauthorized: false
+    }
+}); //creates an email transporter.
+
+const output = `<p>There should be two of these</p>`;
 
 
+let mailOptions = {
+    from: process.env.EMAIL, // sender address
+    to: 'jjrich13@gmail.com', // list of receivers
+    subject: 'This is a demo email and you should receive it twice', // Subject line
+    text: `Demo`, // plain text body
+    html: output // html body
+};
+// END NODEMAILER STUFF THAT YOU MIGHT WANT TO MOVE
 
 router.put('/toggleActive/:id', rejectUnauthenticated, (req, res) => {
     pool.query(`UPDATE users SET active = NOT active WHERE id = $1;`, [req.params.id]
@@ -80,11 +107,59 @@ router.get('/inactiveStudent', rejectUnauthenticated, (req, res) => {
     })
 });
 
-/**
- * POST route template
- */
-router.post('/', (req, res) => {
 
+//this route is for testing
+router.get('/test', (req, res) => {
+    pool.query(
+        `SELECT * FROM users LEFT OUTER JOIN feedback on feedback.id = (SELECT id from feedback
+            WHERE feedback.user_id = users.id
+            ORDER BY feedback.date desc
+            LIMIT 1)
+        WHERE users.active = true and users.resident = false;`
+    ).then( response => {
+        const dateLong = new Date()
+        console.log('dateLong:', dateLong);
+        
+        console.log('response date', response.rows[2].date.toISOString().substring(0,10));
+        
+        
+        let emailArray = []
+        for(response of response.rows){
+            console.log(response.email);
+            if(dateToString(response.date) === dateToString(dateLong)){
+                emailArray.push(response.email)
+            }
+            
+        }
+
+        for(email of emailArray){
+            let sample = {
+                from: process.env.EMAIL, // sender address
+                to: email, // list of receivers
+                subject: 'This is a demo email and you should receive it twice', // Subject line
+                text: `Demo`, // plain text body
+                html: output // html body
+            };
+
+            transporter.sendMail(sample, (error, info) => {
+                if (error) {
+                    res.sendStatus(500);
+                    return console.log(error);
+                }
+                console.log('Message sent: %s', info.messageId);
+                console.log('info rawL ', info);
+                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+                console.log('email has been sent');
+            });
+
+        }
+        
+        res.sendStatus(200)
+    }).catch( err => {
+      console.log(err);
+      res.sendStatus(500);
+    })
 });
+//end test
 
 module.exports = router;
---
+++ server/routes/email.scheduler.js
@@ -0,0 +1,134 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const nodemailer = require('nodemailer');
+const cron = require('node-cron');
+const { dateToString } = require('../modules/dateToString');
+
+// NODEMAILER STUFF THAT YOU MIGHT WANT TO MOVE
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    },
+    // for handling request from local host 
+    tls: {
+        rejectUnauthorized: false
+    }
+}); //creates an email transporter.
+const feedbackReminderEmailHTML = `<p>This is a reminder to submit your feedback for the day.</p>
+    <p>If you are not with your resident feel free to submit without a signature.</p>
+    <a href="trackmyrotation.com">Submit Here</a>`;
+
+const output = `<p>This email should arrive at 5pm</p>`;
+
+
+let mailOptions = {
+    from: process.env.EMAIL, // sender address
+    to: 'jjrich13@gmail.com', // list of receivers
+    subject: 'This is a demo email', // Subject line
+    text: `Sup homeboy?`, // plain text body
+    html: output // html body
+};
+// END NODEMAILER STUFF THAT YOU MIGHT WANT TO MOVE
+
+router.post('/sendEmail', (req, res) => {
+    console.log('hit sendEmail');
+    console.log('Email:', process.env.EMAIL);
+    console.log('Password:', process.env.PASSWORD);
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            res.sendStatus(500);
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('info rawL ', info);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        console.log('email has been sent');
+        res.sendStatus(200);
+    });
+});
+
+//this will run monday-friday at 17:00 or 5pm
+//'0 17 * * 1-5', i.e.: the 0th minute of the 17th hour, everyday of the month, every month, but only monday through friday
+// cron.schedule('0 17 * * 1-5', () => {
+//     pool.query(
+//         `SELECT * FROM users LEFT OUTER JOIN feedback on feedback.id = (SELECT id from feedback
+//             WHERE feedback.user_id = users.id
+//             ORDER BY feedback.date desc
+//             LIMIT 1)
+//         WHERE users.active = true and users.resident = false;`
+//     ).then( response => {
+//         const dateLong = new Date()
+        
+//         let emailArray = []
+//         for(response of response.rows){
+//             console.log(response.email);
+//             if(dateToString(response.date) === dateToString(dateLong)){
+//                 emailArray.push(response.email)
+//             }
+            
+//         }
+
+//         for(email of emailArray){
+//             let sample = {
+//                 from: process.env.EMAIL, // sender address
+//                 to: email, // list of receivers
+//                 subject: 'Feedback Reminder', // Subject line
+//                 text: `Demo`, // plain text body
+//                 html: feedbackReminderEmailHTML // html body
+//             };
+
+//             transporter.sendMail(sample, (error, info) => {
+//                 if (error) {
+//                     res.sendStatus(500);
+//                     return console.log(error);
+//                 }
+//                 console.log('Message sent: %s', info.messageId);
+//                 console.log('info rawL ', info);
+//                 console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+//                 console.log('email has been sent');
+//             });
+
+//         }
+        
+//         res.sendStatus(200)
+//     }).catch( err => {
+//       console.log(err);
+//       res.sendStatus(500);
+//     })
+// });
+
+cron.schedule('0 22 * * 1-5', () => {
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            res.sendStatus(500);
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('info rawL ', info);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        console.log('email has been sent');
+        res.sendStatus(200);
+    });
+});
+
+cron.schedule('0 17 * * 1-5', () => {
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            res.sendStatus(500);
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('info rawL ', info);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        console.log('email has been sent');
+        res.sendStatus(200);
+    });
+});
+
+module.exports = router;
